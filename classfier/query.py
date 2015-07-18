af11160e7c6a84849202f4bee8dfa6fd96d87e79
Jasmeet Singh <jasmeet13n@users.noreply.github.com>
2015-07-18 05:24:19 -0500

    test

--- classfier/query.py
+++ classfier/query.py
@@ -1,30 +1,3 @@
-"""
-===================================================
-Faces recognition example using eigenfaces and SVMs
-===================================================
-
-The dataset used in this example is a preprocessed excerpt of the
-"Labeled Faces in the Wild", aka LFW_:
-
-  http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz (233MB)
-
-.. _LFW: http://vis-www.cs.umass.edu/lfw/
-
-Expected results for the top 5 most represented people in the dataset::
-
-                   precision    recall  f1-score   support
-
-     Ariel Sharon       0.67      0.92      0.77        13
-     Colin Powell       0.75      0.78      0.76        60
-  Donald Rumsfeld       0.78      0.67      0.72        27
-    George W Bush       0.86      0.86      0.86       146
-Gerhard Schroeder       0.76      0.76      0.76        25
-      Hugo Chavez       0.67      0.67      0.67        15
-       Tony Blair       0.81      0.69      0.75        36
-
-      avg / total       0.80      0.80      0.80       322
-
-"""
 from __future__ import print_function
 
 from time import time
@@ -44,17 +17,20 @@ from fetch_data import load_one_image
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
-pca = joblib.load('my_pca.pkl')
-clf = joblib.load('my_model.pkl')
-target_names = joblib.load('target_names.pkl')
+def loadModel():
+  pca = joblib.load('my_pca.pkl')
+  clf = joblib.load('my_model.pkl')
+  target_names = joblib.load('target_names.pkl')
+  return pca, clf, target_names
 
-X=load_one_image(["/home/jasmeet/scikit_learn_data/lfw_home/lfw_funneled/Colin_Powell/pic1.jpg"], slice_=None, color=False, resize=0.4)
+def queryModel(pca, clf, taget_names, img_path=None):
+  pca, clf, target_names = load_model()
+  if img_path == None:
+    img_path = "/home/jasmeet/scikit_learn_data/lfw_home/lfw_funneled/Colin_Powell/pic1.jpg" 
+  
+  X=load_one_image([], slice_=None, color=False, resize=0.4)
+  X_pca = pca.transform(X)
+  y_pred = clf.predict(X_pca)
 
-X_pca = pca.transform(X)
-y_pred = clf.predict(X_pca)
-
-print(str(y_pred))
-print(target_names[y_pred[0]])
-
-#print(classification_report(y, y_pred, target_names=target_names))
-#print(confusion_matrix(y, y_pred, labels=range(n_classes)))
+  print(str(y_pred))
+  print(target_names[y_pred[0]])
